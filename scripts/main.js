28302ac07d904d689a1540f2065a50f955e17da2
Diogo CS Berner <diogoberner@gmail.com>
2025-02-11 22:38:09 -0300

    refatoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
-import { cardToggle } from "./cardToggle.js"
-import updateUI from "./updateUI.js"
+const cardList = document.querySelector(".app__card-list")
 
-cardToggle(".app__card-list", ".app__card-button")
-updateUI("foco")
+cardList.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("app__card-button")) {
+        return
+    }
+
+    const currentButton = e.target
+    if (!currentButton) return
+    const buttonAttribute = currentButton.getAttribute("data-contexto")
+
+    const activeButton = document.querySelector("app__card-button.active")
+
+    if (activeButton && activeButton !== currentButton) {
+        activeButton.classList.remove("active")
+        updateUI(buttonAttribute)
+    }
+
+    currentButton.classList.add("active")
+
+})
